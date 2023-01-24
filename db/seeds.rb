d33ddd4f83ba50cad41506d3b135ef393e3a3e4b
Victoria Maggay <toria0305@gmail.com>
2023-01-24 13:17:44 -0800

    testing branches

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,3 @@ a4 = Attraction.create(location:, rating:, destination_id: .id, user_id: .id)
 a5 = Attraction.create(location:, rating:, destination_id: .id, user_id: .id)
 a6 = Attraction.create(location:, rating:, destination_id: .id, user_id: .id)
 a1 = Attraction.create(location:, rating:, destination_id: .id, user_id: .id)
-Victoria Test
