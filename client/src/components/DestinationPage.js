098fd4155ce06d69ea7d18a29e9b488d39dd4d5d
Ana Rosario <arosario.mrd@gmail.com>
2023-01-26 12:37:28 -0500

    routes

---
+++ client/src/components/DestinationPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function DestinationPage(){
+
+
+
+
+
+    return(
+
+        <div></div>
+    )
+}
+
+
+
+
+
+export default DestinationPage;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
-function navBar() {
-return (
 
 
-)
+function NavBar({ onChangePage }) {
+
+    function handleLinkClick(e) {
+        e.preventDefault()
+        onChangePage(e.target.pathname)
+    }
+
+    return (
+        <nav>
+            <a onClick={handleLinkClick} href="/">Home</a>
+            <a onClick={handleLinkClick} href="/login">Login</a>
+            <a onClick={handleLinkClick} href="/signup">Sign up</a>
+
+        </nav>
+
+);
 }
+
+export default NavBar;
